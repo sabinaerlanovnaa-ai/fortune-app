1f116faeeb5eee8623da18064ce74feee04fa10e
sabinaerlanovna <sabinasultankazyy@gmail.com>
2025-10-09 10:20:03 +0500

    rebuild GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fortune-app/', // <-- добавь эту строку
   plugins: [react()],
-})
+});
